fd13786f5c53d01d676bd9af6d7fef5be43943e0
= <=>
2021-02-23 21:57:00 +0800

    Memuat gambar

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,24 @@ class MyApp extends StatelessWidget {
                     ],
                   ),
                 ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff7c94b6),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://asset-a.grid.id/crop/0x0:0x0/x/photo/2019/06/05/478985708.jpg'),
+                      fit: BoxFit.fitWidth,
+                    ),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  height: 200,
+                  width: 300,
+                  margin: EdgeInsets.all(20),
+                ),
               ],
             )));
   }
